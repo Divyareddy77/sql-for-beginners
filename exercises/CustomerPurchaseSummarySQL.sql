865377311958ceaff6a2f53426ae555d9716b85e
Vanga Divya <vangadivyareddy1210@gmail.com>
2026-05-23 13:32:13 +0530

    ✅ Add solution for: Customer Purchase Summary - SQL

---
+++ exercises/CustomerPurchaseSummarySQL.sql
@@ -0,0 +1,2 @@
+-- Write a SQL query below --
+SELECT id as order_id, ROUND(total_amount,2) AS total_amount, TO_CHAR(ordered_at,'YYYY-MM-DD HH24:MI:SS') AS ordered_at FROM orders WHERE order_status='Delivered' AND total_amount>150.00 ORDER BY ordered_at DESC;
